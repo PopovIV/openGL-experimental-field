e66d73b079f191179c8e775095a96dea2121e8c4
Ivan Popov <71888203+PopovIV@users.noreply.github.com>
2024-06-30 20:09:56 +0400

    + fix indices

--- OpenGL/Main.cpp
+++ OpenGL/Main.cpp
@@ -35,25 +35,6 @@ int main() {
 	// So we only have the modern functions
 	glfwInitHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Vertices array for triangle
-	GLfloat vertices[] =
-	{
-		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,    // lower left corner
-		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,     // lower right corner
-		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f   // upper corner
-		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // inner left
-		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,  // inner right
-		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f      // inner down
-	};
-
-	// Indices array for triangle
-	GLfloat indices[] =
-	{
-		0, 3, 5, // lower left triangle
-		3, 2, 4, // lower right triangle
-		5, 4, 1  // upper triangle
-	};
-
 	// Create window class
 	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDHT, WINDOW_HEIGHT, "OpenGL", NULL, NULL);
 	
@@ -97,6 +78,26 @@ int main() {
 	// Delete shaders
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
+
+
+	// Vertices coordinates
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Upper triangle
+		5, 4, 1 // Lower right triangle
+	};
 
 	// Create vertex array object(VAO)
 	// !! Create always before VBO
